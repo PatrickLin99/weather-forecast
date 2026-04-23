90037e7ee0883d73c3a22499e3ebed49a7a24f17
Patrick Lin <61694830+PatrickLin99@users.noreply.github.com>
2026-04-23 23:46:40 +0800

    feat: add WeatherApp Hilt application class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.weatherapp.android.application)
+    alias(libs.plugins.weatherapp.android.hilt)
 }
 
 android {
@@ -18,6 +19,12 @@ android {
 }
 
 dependencies {
+    implementation(projects.feature.weather)
+    implementation(projects.core.data)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.common)
+    implementation(projects.core.model)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -26,6 +33,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
